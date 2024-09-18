f2123e259e82c1b0abf61fd6b0bbd8f79e790713
Kathircoder <bkathiravan01@gmail.com>
2024-09-18 10:16:21 +0530

    updated

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -1,11 +1,21 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
+    const data = async function getData() {
+        const request = await fetch('https://66e526db5cc7f9b6273c6ab5.mockapi.io/users');
+        const response = await request.json();
+        return response;
+    }
     return (
         <>
-            <div className="h-screen w-screen flex justify-center items-center bg-purple-500">
-                <div className="h-[50%] w-[30%] flex justify-center items-center bg-white rounded-md">
+        <div className="flex flex-col justify-start items-center">
+            <Navbar Data = {data} />
+        </div>
+            <div className="h-screen w-screen flex justify-center items-center bg-[#f5f5f5]">
+                <div className="h-[50%] w-[30%] flex justify-center items-center shadow-md bg-white rounded-md hover:h-[51%] hover:w-[31%]">
                     <form action="" className="h-[80%] w-[80%] flex flex-col justify-center items-center gap-4">
-                         <input type="text" name="" className="p-3 w-full bg-[#f8f8f8] rounded-md outline-none focus:border-b-2 hover:border-purple-500 font-bold " id="" placeholder="Name" />
-                         <input type="tel" name="" className="p-3 w-full bg-[#f8f8f8] font-bold rounded-md outline-none focus:border-b-2 hover:border-purple-500" id="" placeholder="Phone"/>
+                         <input type="text" name="" className="p-3 w-full bg-[#f8f8f8] rounded-md outline-none focus:border-2 hover:border-purple-500 font-bold " id="" placeholder="Name" />
+                         <input type="tel" name="" className="p-3 w-full bg-[#f8f8f8] font-bold rounded-md outline-none focus:border-2 hover:border-purple-500" id="" placeholder="Phone"/>
                         <button className="p-3 bg-purple-500 w-full text-white rounded-md font-bold"> Submit</button>
                     </form>
                 </div>
---
+++ sample/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = ({Data}) => {
+    console.log(Data.isAlive);
+    return (
+        <div className="w-full h-[4vh] text-white p-5 text-lg bg-purple-500 font-bold flex justify-center items-center">
+            Hello
+        </div>
+    )
+}
+export default Navbar;
