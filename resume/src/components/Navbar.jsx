e42749355f66cc0552efb29a1b8c125312ae49de
Kathircoder <bkathiravan01@gmail.com>
2024-09-18 21:23:01 +0530

    included-contact page

--- resume/src/components/Navbar.jsx
+++ resume/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { Cross, User2 } from 'lucide-react'
-import React, { useState } from 'react'
+import { XCircle, User2 } from 'lucide-react'
+import React, { useRef, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
@@ -18,6 +18,14 @@ const Navbar = () => {
             url: "/contact"
         }
     ]
+
+    const emailref = useRef(null)
+    const passwordref = useRef(null)
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        console.log(emailref.current.value, passwordref.current.value)
+    }
     const [visible, setvisible] = useState(false)
     return (
         <>
@@ -51,19 +59,19 @@ const Navbar = () => {
                 visible && (
                     <>
                         <div className="h-screen w-screen absolute top-0 left-0 bg-black/20 flex justify-center items-center z-50">
-                            <div className=" h-[40%] w-[30%] bg-red z-50 flex flex-col  bg-white  shadow-lg ">
-                                <div className="w-full h-[15%] flex flex-row justify-start px-10 items-center border-2 text-white bg-purple-500 text-xl font-bold ">
+                            <div className=" h-[40%] w-[30%] bg-red z-50 flex flex-col  bg-white rounded-md  shadow-lg ">
+                                <div className="w-full h-[15%] flex flex-row justify-start px-10 items-center border-2 text-white bg-blue-500 rounded-md text-xl font-bold ">
                                     <div className="w-1/2">
                                         Login
                                     </div>
                                     <div className="w-1/2 flex justify-end">
-                                        <Cross onClick={() => setvisible(!visible)} />
+                                        <XCircle className='hover:cursor-pointer' onClick={() => setvisible(!visible)} />
                                     </div>
                                 </div>
                                 <div className="w-full h-[85%] flex flex-row justify-center items-center">
-                                    <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4">
-                                        <input type="email"  name="" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required />
-                                        <input type="password"  name="" id="password" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required />
+                                    <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4" onSubmit={handleLogin}>
+                                        <input type="email" ref={emailref}  name="" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required />
+                                        <input type="password" ref={passwordref}  name="" id="password" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required />
                                         {/* <input type="email" name="" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange} />
                                         <input type="password" name="" id="password" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange} /> */}
                                         <button type="submit" className="bg-green-500 p-3 text-white w-full rounded-sm text-lg font-bold">   Login</button>
--- resume/src/pages/Contact.jsx
+++ resume/src/pages/Contact.jsx
@@ -1,11 +1,40 @@
-import { useState } from "react"
-
+import { User } from 'lucide-react'
+import { useRef } from 'react'
 
 const Contact = () => {
-  const [visible, setvisible] = useState(false)
+
+  const firstNameref = useRef(null)
+  const lastNameref = useRef(null)
+  const emailref = useRef(null)
+  const phoneref = useRef(null)
+  const messageref = useRef(null)
+
+  const contactReport = (e) => {
+
+    e.preventDefault()
+
+    console.log(firstNameref.current.value)
+    console.log(lastNameref.current.value)
+    console.log(emailref.current.value)
+    console.log(phoneref.current.value)
+    console.log(messageref.current.value)
+  }
   return (
     <>
-      <div>Contact</div>
+         <div className="h-screen w-screen flex justify-center items-center">
+            <div className="h-[60%] w-[30%] flex justify-center items-center bg-[#f8f8f8] rounded-md shadow-md">
+              <form action="" className="h-full w-full flex flex-col justify-center items-center space-y-5" onSubmit={ contactReport }>
+                <div className="h-[3rem] w-[17rem] flex flex-row justify-center items-center space-x-4">
+                  <input ref={ firstNameref } className="w-[8rem] h-[3rem] p-3 border-2 text-xl rounded-md" type="text" name="" placeholder="First-name" id="" />
+                  <input ref={ lastNameref } type="text" className="w-[8rem] h-[3rem] p-3 border-2 text-xl rounded-md" name="" placeholder="Last-name" id="" />
+                </div>
+                  <input ref={ emailref } type="email" className="h-[3rem] w-[17rem] p-3 border-2 text-xl rounded-md" placeholder="email" name="" id="" />
+                  <input ref={ phoneref } type="tel" className="h-[3rem] w-[17rem] p-3 border-2 text-xl rounded-md" name="" placeholder="Phone" id="" />
+                  <input ref={ messageref } type="text" className="h-[3rem] w-[17rem] p-3 border-2 text-xl rounded-md text-gray-500" name="" placeholder="Message" id="" />
+                  <button className="h-[3rem] w-[17rem] border-b bg-blue-500 font-bold text-xl text-white rounded-md hover:bg-blue-600">Send</button>
+              </form>
+            </div>
+         </div>
     </>
   )
 }
