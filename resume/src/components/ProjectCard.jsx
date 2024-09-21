52159be13d46f0eae259f884308a71e397300ad0
vasanthkrishnan <vasanthakrishnans22it@srishakthi.ac.in>
2024-09-21 22:59:09 +0530

    updated-projects

---
+++ resume/src/components/ProjectCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ProjectCard = () => {
+  return (
+        <>
+            
+        </>
+  )
+}
+
+export default ProjectCard
+
--- resume/src/pages/Profile.jsx
+++ resume/src/pages/Profile.jsx
@@ -9,8 +9,8 @@ const Profile = () => {
                         <div className='h-[90%] w-[80%] mx-[6rem] my-[5rem] flex flex-col'>
                             <div className='h-[4rem] w-[15rem] mx-[2rem] my-[.5rem] flex justify-start items-center text-black font-bold text-5xl font-serif'>Hello!</div>
                             <div className='h-[5rem] w-[40rem] mx-[2rem] my-[.5rem] flex justify-start items-center text-red-700 font-bold text-5xl font-serif '>I'm<span className='text-black font-serif ml-8'>Vasantha Krishnan S</span></div>
-                            <div className='h-[22rem] w-[90%] mx-[2rem] my-[.5rem] flex justify-center items-start text-center text-black font-bold font-serif text-4xl '>
-                            I am an engineering student skilled in Java, React, MongoDB, and front-end tools. I focus on building scalable, efficient, and user-friendly applications, with a passion for continuous learning and staying current with the latest industry trends and technologies.</div>
+                            <div className='h-[22rem] w-[90%] mx-[2rem] my-[5rem] flex justify-center items-start text-center text-black font-bold font-serif text-2xl '>
+                            Passionate software developer with expertise in Java, JavaScript, and problem-solving. Skilled in algorithms, web development, and creating efficient, user-friendly applications. Eager to learn and innovate.</div>
                             
                         </div>
                     </div>
